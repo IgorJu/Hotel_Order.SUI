4de326a3315430585f119f96bb801a2bc707908a
Igor Rukaleev <575714470@mail.ru>
2023-09-09 18:43:54 +0300

    Initial Commit

---
+++ Hotel_TestTask_SUI/Hotel_TestTask_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hotel_TestTask_SUIApp.swift
+//  Hotel_TestTask_SUI
+//
+//  Created by Igor on 09.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hotel_TestTask_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
